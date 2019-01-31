a803c96908b04d44d87433637abb6941ef168637
arsanuos <arsanuos.attia@gmail.com>
2019-01-31 02:41:45 +0200

    adding visibility conditions

--- imports/ui/components/menu/MenuTable.js
+++ imports/ui/components/menu/MenuTable.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
 
-export default class MenuTable extends Component{
+class MenuTable extends Component{
 
     render(){
         let computedWidth = 100/(3 - 1) + " %";
@@ -10,18 +11,27 @@ export default class MenuTable extends Component{
         let options = this.props.options;
         return (
             <BootstrapTable data={data} cellEdit={ cellEditProp } striped={true} hover={true} height='700'
-                    scrollTop={ 'Bottom' } pagination search deleteRow={ true } 
-                    selectRow={ { mode: 'checkbox' } } insertRow={ true } exportCSV={ true }
+                    scrollTop={ 'Bottom' } pagination search deleteRow={ this.props.isWorker } 
+                    selectRow={ this.props.isWorker ? { mode: 'checkbox' } : {} } insertRow={ this.props.isWorker } exportCSV={ true }
                     hover options={ options } keyField='_id' trClassName={rowClassNameFormat}>    
                     
                     <TableHeaderColumn dataField='_id' 
                         width={computedWidth} dataSort={true} hidden autoValue>Id</TableHeaderColumn>
                     <TableHeaderColumn dataField='name' 
-                        width={computedWidth} isKey={false} dataSort={true}>Name</TableHeaderColumn>
+                        width={computedWidth} isKey={false} editable={this.props.isWorker} dataSort={true}>Name</TableHeaderColumn>
                     <TableHeaderColumn dataField='price'
-                        width={computedWidth} isKey={false} dataSort={true}>Price</TableHeaderColumn>
+                        width={computedWidth} isKey={false} editable={this.props.isWorker} dataSort={true}>Price</TableHeaderColumn>
         
             </BootstrapTable>
         );
     }
 }
+
+export default withTracker((props) => {
+
+    return {
+        isAdmin: Roles.userIsInRole(Meteor.userId(), 'admin', 'default-group'),
+        isUser: Roles.userIsInRole(Meteor.userId(), 'user', 'default-group'),
+        isWorker: Roles.userIsInRole(Meteor.userId(), 'worker', 'default-group'),
+    }
+})(MenuTable);
